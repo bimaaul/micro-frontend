31df851c78e1cfda4dda5ecdaa521fcc3c2f7a7c
Bima Aulia <bimaaulia.15@gmail.com>
2024-02-26 00:04:27 +0700

    feat: implement lazy load and add default path

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,12 @@
-import React from "react"
-import { BrowserRouter } from "react-router-dom"
+import React, { lazy, Suspense } from "react"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import { StylesProvider, createGenerateClassName } from "@material-ui/core"
 
 import Header from "./components/Header"
-import MarketingApp from "./components/MarketingApp"
-import { StylesProvider, createGenerateClassName } from "@material-ui/core"
+import ProgressBar from "./components/ProgressBar"
+
+const MarketingLazy = lazy(() => import("./components/MarketingApp"))
+const AuthLazy = lazy(() => import("./components/AuthApp"))
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
@@ -14,7 +17,12 @@ export default () => {
     <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
         <Header />
-        <MarketingApp />
+        <Suspense fallback={<ProgressBar />}>
+          <Switch>
+            <Route path="/auth" component={AuthLazy} />
+            <Route path="/" component={MarketingLazy} />
+          </Switch>
+        </Suspense>
       </BrowserRouter>
     </StylesProvider>
   )
